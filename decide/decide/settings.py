55af6ddd034cd084a5396dcb095a2b0a4108917e
enrjorarr <enrjorarr@alum.us.es>
2021-02-11 18:08:46 +0100

    Añadidos los archivos necesarios para desplegar en heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -153,6 +153,8 @@ STATIC_URL = '/static/'
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
 
+BASEURL = "https://examen830.herokuapp.com/"
+
 try:
     from local_settings import *
 except ImportError:
@@ -160,3 +162,6 @@ except ImportError:
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
